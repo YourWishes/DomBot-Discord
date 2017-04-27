cc8944db9a9c78232c3dfc5e37a04f2825b7ced9
Dominic Masters <dominic@domsplace.com>
2017-04-27 14:44:36 +0930

    Delet this
    Not meant to be packaged!

--- src/commands/bot_commands/SpeakCommand.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    label: "speak",
-    command: function(label, args, scope) {
-		let guild = scope.channel.guild;
-		let dombot = guild.dombot;
-		dombot.sendMessage(args.join(' '));
-    }
-};
